258a5f44423a2a4a0633160624862448af2c4a49
Pol de la Hoz <poldelahoz@gmail.com>
2023-02-21 14:40:23 +0100

    T02N01

---
+++ S04T02N01DelahozGaigPol/src/main/java/cat/itacademy/barcelonactiva/delahoz/pol/s04/t02/n01/S04T02N01DelahozGaigPol/model/domain/Fruita.java
@@ -0,0 +1,60 @@
+package cat.itacademy.barcelonactiva.delahoz.pol.s04.t02.n01.S04T02N01DelahozGaigPol.model.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "fruites")
+public class Fruita {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name = "nom")
+	private String nom;
+	
+	@Column(name = "quantitatQuilos")
+	private int quantitatQuilos;
+	
+	public Fruita() {}
+	
+	public Fruita(String nom, int quantitatQuilos) {
+		this.nom = nom;
+		this.quantitatQuilos = quantitatQuilos;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getQuantitatQuilos() {
+		return quantitatQuilos;
+	}
+
+	public void setQuantitatQuilos(int quantitatQuilos) {
+		this.quantitatQuilos = quantitatQuilos;
+	}
+	
+	@Override
+	public String toString() {
+		return "Fruita [id=" + id + ", nom=" + nom + ", quantitatQuilos=" + quantitatQuilos + "]";
+	}
+	
+}
